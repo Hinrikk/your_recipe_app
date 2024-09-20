22512be27ed7a02a6e115337219782e080e635aa
Hinrik Könnecker <hinrik.koennecker@gmail.com>
2024-09-20 23:37:26 +0200

    starter

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { Image, StyleSheet, Platform, Text } from "react-native";
+import { ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>Hallo</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+});
